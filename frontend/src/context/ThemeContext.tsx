6c8407b739e92f34315e9a36ec21e4a5433e0601
PmMichail <sprign12@gmail.com>
2026-05-25 06:58:32 +0300

    qwa122

--- frontend/src/context/ThemeContext.tsx
+++ frontend/src/context/ThemeContext.tsx
@@ -9,7 +9,7 @@ export const lightTheme = {
   textSecondary: '#8B756E',
   primary: '#B87460',
   border: '#EADAD2',
-  fontFamily: 'Poppins-Regular'
+  fontFamily: Platform.OS === 'ios' ? 'System' : 'Roboto'
 };
 
 export const darkTheme = {
@@ -19,7 +19,7 @@ export const darkTheme = {
   textSecondary: '#D7C2B9',
   primary: '#E8B9A7',
   border: '#44312C',
-  fontFamily: 'Poppins-Regular'
+  fontFamily: Platform.OS === 'ios' ? 'System' : 'Roboto'
 };
 
 type ThemeContextType = {
